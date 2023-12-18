4ea4ff68a01c58e10aa3c338a2ab2c34fca49e54
Vitor L. <vitoribeiro0024@gmail.com>
2023-12-17 21:12:47 -0300

    SQL Ingestion configured.

---
+++ src/dash/main.py
@@ -0,0 +1,12 @@
+from dash import Dash
+
+
+def main() -> None:
+    app = Dash()
+    app.title = "de Mariana Dashboard"
+    app.layout = ""
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
