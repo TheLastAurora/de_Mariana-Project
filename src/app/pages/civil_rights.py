78303038282cf495e42f7c323a4725cabc4c4ee0
Vitor L. <vitoribeiro0024@gmail.com>
2024-01-03 15:22:49 -0300

    Added Property Rights Index

--- src/app/pages/civil_rights.py
+++ src/app/pages/civil_rights.py
@@ -1,29 +1,23 @@
 from dash import dcc, callback
 import dash_bootstrap_components as dbc
 from dash import html
+from math import floor
 from dash.dependencies import Input, Output, State
 import polars as pl
 import plotly.express as px
+
+# Dags importing
 import taxes
+import civil_rights
 
 tax_per_country_df = taxes.tax_per_country()
+property_rights_df = civil_rights.property_rights_ranking()
 
 layout = dbc.Container(
     [
         html.H2(id="selected-country", style={"font-weight": "bold"}),
         html.Div(
-            [
-                dbc.Card(
-                    dbc.CardBody(
-                        [
-                            html.H3("34th", style={"font-weight": "bold"}),
-                            html.H4("WGI Overral Index"),
-                            html.P("Among the worst 46%"),
-                        ],
-                    ),
-                    className="info-card text-center",
-                ),
-            ],
+            [dbc.Card(id="property-rights-card")],
             className="cards-div",
             style={"display": "flex", "flex-direction": "row"},
         ),
@@ -32,22 +26,53 @@ layout = dbc.Container(
 )
 
 
-# @callback(
-#     Output("taxes-year-graph", "figure"),
-#     Input("selected-country", "value"),
-# )
-# def update_taxes_country(country_id: int):
-#     df = tax_per_country_df.filter(pl.col("country_id") == country_id)
-#     fig = px.line(
-#         df,
-#         x="year",
-#         y=["mean_tariff_rate", "standard_deviation_of_tariff_rates", "tariffs"],
-#     )
-#     fig.update_layout(xaxis_title="Year", yaxis_title="Tariffs")
-#     fig.update_xaxes(tickvals=df["year"].unique(), tickmode="array")
-#     return fig
+@callback(
+    Output("property-rights-card", "children"), Input("selected-country", "value")
+)
+def property_rights(country_id: str) -> dbc.Card:
+    """Extracting for the specific country"""
+    df = property_rights_df
+    try:
+        ranking = df.select(pl.arg_where(pl.col("country_id") == country_id)).item()
+        status = "best" if ranking <= floor(len(df) / 2) else "worst"
+        percentage = (
+            ranking / len(df) * 100
+            if status is "best"
+            else 100 - ranking / len(df) * 100
+        )
+        comparison = (
+            "Among the **{:.1f}% {}** countries in safeguarding this right.".format(
+                percentage, status
+            )
+        )
+        return dbc.Card(
+            dbc.CardBody(
+                [
+                    html.H3(f"{ranking}th", style={"font-weight": "bold"}),
+                    html.H4("Property Rights Index"),
+                    dcc.Markdown(comparison),
+                ],
+            ),
+            className="info-card text-center",
+        )
+
+    except ValueError:
+        return dbc.Card(
+            dbc.CardBody(
+                [
+                    html.H4(
+                        "No information on Property Rights avaliable for this Country.",
+                        style={"font-weight": "bold"},
+                    ),
+                ],
+            ),
+            className="info-card text-center",
+        )
 
 
-# @callback()
-# def test():
-#     pass
+def freedom_of_labour_right():
+    pass
+
+
+def freedom_of_expression_right():
+    pass
--- src/dags/civil_rights.py
+++ src/dags/civil_rights.py
@@ -1,5 +1,7 @@
 import sys
 import os
+from math import floor
+from typing import Tuple
 
 current_folder = os.path.dirname(os.path.abspath(__file__))
 dags_path = os.path.join(current_folder, "..", "repository")
@@ -15,14 +17,67 @@ conn_ef = database.DatabaseConnection("economic-freedom").connection
 conn_db = database.DatabaseConnection("doing-bussiness").connection
 
 
-def property_rights() -> pl.DataFrame:
-    wgi = pl.read_database(
+def property_rights_ranking() -> pl.DataFrame:
+    db_rank_registering_property = pl.read_database(
         query="""
-        SELECT name, year, estimate, rank
-        FROM RuleOfLaw 
-        LEFT JOIN iso_codes
+        SELECT country_id, rank_registering_property
+        FROM "freedom"
+        WHERE YEAR = 2020
+        ORDER BY rank_registering_property
         """,
-        connection=conn_wgi
+        connection=conn_db,
     )
-    
-# TODO: Start working with real logic!!!
+
+    ef_property_rights = pl.read_database(
+        query="""
+            SELECT country_id, real_property, property_rights
+            FROM "freedom"
+            WHERE YEAR = 2020
+            ORDER BY country_id
+            """,
+        connection=conn_ef,
+    )
+
+    ti_property_rights = pl.read_database(
+        query="""
+            SELECT country_id, property_rights, private_property
+            FROM "freedom"
+            WHERE YEAR = 2020
+            ORDER BY country_id
+            """,
+        connection=conn_ti,
+    )
+
+    ef_weights = [1.5, 1.0]
+    ti_weights = [0.5, 0.5]
+    db_weight = 3.5
+    ef_property_rights = ef_property_rights.with_columns(
+        pl.fold(
+            acc=pl.lit(0),
+            function=lambda c1, c2: c1 + c2,
+            exprs=[
+                pl.col(col) * pl.lit(wgt)
+                for col, wgt in zip(ef_property_rights.columns[1:], ef_weights)
+            ],
+        ).alias("score")
+    ).sort(by="score", descending=True)
+
+    ef_property_rights = ef_property_rights.with_columns(
+        ef_rank=ef_property_rights["score"].rank("ordinal", descending=True)
+    ).select(pl.col(["country_id", "ef_rank"]))
+
+    ti_property_rights = ti_property_rights.with_columns(
+        pl.sum_horizontal(pl.exclude("country_id")).rank("ordinal").alias("ti_rank")
+    ).select(pl.col(["country_id", "ti_rank"]))
+
+    db_rank_registering_property = db_rank_registering_property.rename(
+        {"rank_registering_property": "db_rank"}
+    )
+
+    df = db_rank_registering_property.join(
+        ef_property_rights, how="left", on="country_id"
+    ).join(ti_property_rights, how="left", on="country_id")
+
+    return df.sort(
+        by=["ef_rank", "db_rank", "ti_rank"], descending=False, nulls_last=True
+    )
